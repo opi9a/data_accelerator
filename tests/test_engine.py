ccb5befb9e84d63e579b7f9abbc7685cde3a6e4b
Roberts <GRoberta@ims.gov.uk>
2017-07-17 21:12:21 +0100

    moved spender() and get_forecast() functions to spend_engine_funcs.py, and added simple unit test in tests/test_engine.py

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2,11 +2,21 @@
 """
 Created on Mon Jul 17 14:15:09 2017
 
-@author: GRoberta
+To run these tests either just run from this file or use following command 
+from main directory:
+    
+    $ python -m pytest .
+    
+Or, if make is installed:
+    
+    $ make test
+    
+should work
+
 """
 
 from spend_engine_funcs import *
 
 def test_spend_engine():
-    test_profile2 = np.array([1,2,5,10,11,12,12,0])
-    assert sum(get_forecast(test_profile2, 0,10,0.1,-0.1,1,0,15)) == 816.38813156100048
+    test_profile2 = np.array([1,2,5,10,11,12,12,1])
+    assert sum(get_forecast(test_profile2, 0,10,0.1,-0.1,1,0,15)) == 853.6565405610005
