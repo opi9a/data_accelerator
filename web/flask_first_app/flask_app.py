8227ecef3a80034a639b63c8f8682cf95b242485
Roberts <GRoberta@ims.gov.uk>
2017-08-03 12:52:10 +0100

    added test_app

--- web/flask_first_app/flask_app.py
+++ web/flask_first_app/flask_app.py
@@ -3,13 +3,15 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-import pandas as pd
-import numpy as np
-from spend_engine_funcs import get_forecast
+# import pandas as pd
+# import numpy as np
+# from spend_engine_funcs import get_forecast
 
 class MyForm(Form):
 	name = StringField('And you are?')
 	submit = SubmitField('Submit')
+
+app = Flask(__name__)
 
 # first functionality:
 # 	- input form, takes profile (csv file path) plus other data reqd
@@ -18,18 +20,11 @@ class MyForm(Form):
 #	- serves a page with the .img (next to input form)
 
 # first just input directly
-test_profile2 = np.array([1,2,5,10,11,12,12,1])
+# test_profile2 = np.array([1,2,5,10,11,12,12,1])
 
-x = get_forecast(test_profile2, 2001, 2005, 0,0,1, 2001, 2010)
+# x = get_forecast(test_profile2, 2001, 2005, 0,0,1, 2001, 2010)
 
-
-app = Flask(__name__)
-
-'''Can put back-end logic, calls to get_forecast() etc, 
-in here
-'''
-
-test_input = "The sum of the profile is  " + str(x)  # passed into template
+# test_input = "The sum of the profile is  " + str(x)  # passed into template
 
 # observe how this is picked up in template html file, at {{ test_input }}
 
@@ -38,9 +33,10 @@ def home():
 	content="this is the home page"
 	return render_template('test_block.html', 
 							content=content,
-							prof = test_profile2, 
-							out_ser=x,
-							out_sum=str(sum(x)))
+							# prof = test_profile2, 
+							# out_ser=x,
+							# out_sum=str(sum(x))
+							)
 
 
 @app.route('/sim/', methods=['GET', 'POST'])
