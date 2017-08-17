e6f89267b9f42ea9df3d4c48b8df13a264bd6ab0
Roberts <GRoberta@ims.gov.uk>
2017-08-17 17:08:25 +0100

    sorted some of issues with joining dfs.  Still need to resolve issue that futures give a series, so can't be joined to actual.

--- RuleSet.py
+++ RuleSet.py
@@ -19,14 +19,9 @@ class RuleSet:
         self.past = None
         self.fut = None
         self.join_output=join_output
+        self.joined = None
         
-        self._info = {"name": self.name,
-             "index_dims": self.index_dims,
-             "index_selection": self.index_selection,
-             "r_func": self.r_func,
-             "pr_args": max(self.r_args),
-             "join_output": self.join_output}
-        
+       
     def get_arg_keys(self):
         #todo: parse out in best way
         return inspect.getfullargspec(self.r_func)[4]
@@ -36,13 +31,46 @@ class RuleSet:
                         self.parent_df, **self.index_selection),:]
         self.fut = self.r_func(self.past, n_pers, **self.r_args)
         if self.join_output:
-            return self.past.sum().join(self.fut)
-        else:
-            return self.fut.sum()
+            self.joined = self.past.join(self.fut)
         
     def __repr__(self):
         outlist = []
+        self._info = {"name": self.name,
+             "index_dims": self.index_dims,
+             "index_selection": self.index_selection,
+             "r_func": self.r_func,
+             "r_args keys": list(self.r_args.keys()),
+             "past type": type(self.past),
+             "fut type": type(self.fut),
+             "join_output": self.join_output}
         for key in self._info:
             temp_string = key.ljust(27) + str(self._info[key]).rjust(10)
             outlist.append(temp_string)
         return "\n".join(outlist)
+    
+if __name__ == "__main__":
+    df = pd.read_pickle('c:/Users/groberta/Work/data_accelerator/spend_data_proc/dfs/main_unstacked_17AUG.pkl')
+    prof1 = np.genfromtxt('c:/Users/groberta/Work/data_accelerator/profiles/prof1.csv')
+    cutoff = pd.Period('3-2014', freq='M')
+    
+    test=2
+    
+    if test == 1:    
+        test_r = RuleSet(df, 't1')
+        test_r.index_selection = {'biol':True}
+        test_r.r_func = r_funcs.r_profile
+        test_r.r_args = {'profile':prof1, 'gen_mult':0.5}
+        print(test_r)
+        test_r.xtrap(120)
+
+    if test == 2:    
+        test_r = RuleSet(df, 't2')
+        test_r.index_selection = {'start_date':slice(cutoff,None,None)}
+        test_r.r_func = r_funcs.r_fut
+        test_r.r_args = {'profile':prof1, 'cutoff_date':cutoff, 
+                            'coh_growth':0, 'term_growth':0, 'name':'fut'}
+        print(test_r)
+        test_r.xtrap(120)
+    
+    
+    
