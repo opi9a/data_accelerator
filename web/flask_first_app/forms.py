6b194429b84d4263580ab07dacd0038d5748773f
Roberts <GRoberta@ims.gov.uk>
2017-08-23 12:20:17 +0100

    tidying

--- web/flask_first_app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form 
-from wtforms.fields import TextField, SubmitField
-from wtforms.validators import Required
-
-class ContactForm(Form):
-    profile = TextField("Profile", [Required("Enter a profile")])
-    submit = SubmitField("Send")
